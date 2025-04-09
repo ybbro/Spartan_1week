c4f92d231c006114852b5474bb7ddc05ca8ea571
ybbro <uvinjjang@naver.com>
2025-04-09 12:52:48 +0900

    2022.3.17f 버전으로 통일 및 작업한 씬 모음
    잘 되면 좋겠어요

---
+++ Assets/Scripts/ProfileTitle.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProfileTitle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void BackToTitle_Button()
+    {
+        SceneManager.LoadScene("StartScene"); 
+    }
+}
