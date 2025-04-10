08b060a2dc5d0f155514d01ff3b54232b492c362
lsh000219 <147456856+lsh000219@users.noreply.github.com>
2025-04-10 19:14:28 +0900

    0410, 19:14

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -78,7 +78,7 @@ public class Board : MonoBehaviour
             float x = -3.0f + (wgap + 0.5f) + (i % (int)w) * (wgap + 1.0f);   // 가로 w
             float y = -3.5f + (hgap + 0.5f) + (int)(i / (int)w) * (hgap + 1.0f);   // 세로 h
             go.transform.position = deckPosition;
-            float delay = i * 0.3f;
+            float delay = i * 0.03f;
             if(i+1 == deckLength) { last = true; }
 
             //go.transform.position = new Vector2(x, y);
--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -32,7 +32,7 @@ public class Card : MonoBehaviour
         
         if (timer > delay)
         {
-            transform.position = Vector3.MoveTowards(transform.position, cardVector, Time.deltaTime * 2);
+            transform.position = Vector3.MoveTowards(transform.position, cardVector, Time.deltaTime * 10);
             
         }
         else { timer += Time.deltaTime; }
