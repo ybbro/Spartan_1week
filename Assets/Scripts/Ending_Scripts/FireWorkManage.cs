f27ced63f3499f6c096ac1206cbeb03b5226fffb
ybbro <uvinjjang@naver.com>
2025-04-10 19:29:04 +0900

    유빈 작업 도중 업로드하여 머지
    1) 히든 미션으로 팀원님 정보 해금한다는 계획이 있었으니 이에 맞춰 팀원 정보 씬 수정
    
    2) 오디오
    (1) 구한 저작권 Free 오디오 클립 프로젝트에 추가
    
    (2) 모든 스크립트에서 적재적소에 쉽게 사용할 수 있게끔 정리
    
    (3) TitleScene에서 브금이 나오지 않으니 조치 완료
    
    (4) 스크럼 시간에 구한 사운드 리소스에 대한 평가 받기
    >> 부정적 평가를 얻은 리소스들에 대해 다른 리소스를 구하는 시간 가지기
    
    (5) 팀원님들 작업이 끝나고 프로젝트를 합치면 해당 부분에 들어가는 bgm, sfx 넣기
    
    3) 엔딩 씬 쓸쓸해요... 성공/실패 브금 및 버튼 변경 연출해보기
    (1) MainScene끝날 때 성공/실패를 엔딩씬으로 넘겨주어 출현 버튼이 달라지게끔 조치
    
    (2) 불꽃놀이 효과 만드는 중

---
+++ Assets/Scripts/Ending_Scripts/FireWorkManage.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FireWorkManage : MonoBehaviour
+{
+    public Transform fireWorkEffect;
+    public int effectNum = 3;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/StageButton.cs
+++ Assets/Scripts/StageButton.cs
@@ -2,43 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class StageButton : MonoBehaviour
 {
     public int stageNum;
-    public Button button;
 
-    public void Start()
-    {
-        // 다음 스테이지 버튼이 해금되지 않는 이슈 해결 !
-        // 기존 : int bestStage = PlayerPrefs.GetInt("bestStage",1);
-        
-        // 문제 발생 이유
-        // 스테이지 클리어 때 "bestStage" 에는 클리어한 최고 스테이지를 저장. 예) 1스테이지 클리어 시 1 저장
-        // 그러면 기존 식에서는 int bestStage = 1 의 값이 들어옵니다.
-        
-        // 아래의 if문에서 if(stageNum > 1) >> 2스테이지부터 버튼 비활성화
-        // 1스테이지를 클리어 하였더라도 2스테이지 버튼이 활성화되지 않게 됩니다.
-
-        // 변경 : 클리어한 최고 스테이지의 다음 스테이지를 지칭하게끔
-        // 1스테이지 클리어 >> +1을 통해 최고 클리어 다음 스테이지 값인 2
-        // if(stageNum > 2) >> 3스테이지 버튼부터 비활성화 >> 1, 2 스테이지 버튼이 활성화
-        int nextStage = PlayerPrefs.GetInt("bestStage", 0) + 1;
-
-        if (stageNum > nextStage)
-        {
-            button.interactable = false; // 버튼 기능 비활성화
-        }
-    }
     public void SelectStage()
     {
-        PlayerPrefs.SetInt("stage", stageNum);
-        SceneManager.LoadScene("MainScene");
-    }
-    public void NextStage()
-    {
-        stageNum++;
         PlayerPrefs.SetInt("stage", stageNum);
         SceneManager.LoadScene("MainScene");
     }
